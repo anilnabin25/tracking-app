d4dbf58858b3348fdbc7e09d08fdeef41b61012c
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2022-04-28 18:47:01 +0545

    Feature/1 location retrival (#1)
    * location retrival code added
    
    * location code added

--- App.tsx
+++ App.tsx
@@ -1,72 +1,84 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button, Linking, Alert} from 'react-native';
+import {StyleSheet, View, Text, Button, Alert} from 'react-native';
 import RNLocation from 'react-native-location';
+import axios from 'axios';
 
-RNLocation.configure({distanceFilter: 5,})
-
+RNLocation.configure({distanceFilter: 5});
 
 const App = () => {
-    const [viewLocation, isViewLocation] = useState([0, 0])
+    const [viewLocation, isViewLocation] = useState({
+        latitude: undefined,
+        longitude: undefined
+    });
+
+
     const uploadLocation = () => {
         try {
-            console.log(viewLocation)
+            console.log(viewLocation);
+
+            let params = {location: viewLocation}
+            // Make a request for a user with a given ID
+            axios.get('http://tracking.divyashr.com.np/', {params: params})
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+
+
         } catch (error) {
             console.log('Something went wrong');
         }
-    }
+    };
 
     const getLocation = async () => {
         let permission = await RNLocation.checkPermission({
             ios: 'whenInUse', // or 'always'
             android: {
-                detail: 'coarse' // or 'fine'
-            }
+                detail: 'coarse', // or 'fine'
+            },
         });
-        console.log(permission)
+        console.log(permission);
         if (!permission) {
             permission = await RNLocation.requestPermission({
-                ios: "whenInUse",
+                ios: 'whenInUse',
                 android: {
-                    detail: "coarse",
+                    detail: 'coarse',
                     rationale: {
-                        title: "We need to access your location",
-                        message: "We use your location to show where you are on the map",
-                        buttonPositive: "OK",
-                        buttonNegative: "Cancel"
-                    }
-                }
-            })
-            console.log(permission)
-
+                        title: 'We need to access your location',
+                        message: 'We use your location to show where you are on the map',
+                        buttonPositive: 'OK',
+                        buttonNegative: 'Cancel',
+                    },
+                },
+            });
+            console.log(permission);
         }
-        let location = await RNLocation.getLatestLocation({timeout: 100})
-        // RNLocation.subscribeToLocationUpdates(location => {
-        //     const loc = location[0].toString()
-        //     console.warn(loc);
-        //     Alert.alert(loc);
-        // });
-        console.log(location)
-        isViewLocation(location)
-    }
-
+        RNLocation.subscribeToLocationUpdates(subLocation => {
+            const loc = JSON.stringify(subLocation[0]);
+            Alert.alert('outside');
+            console.warn(loc);
+            // @ts-ignore
+            isViewLocation(subLocation[0]);
+        });
+    };
 
     return (
         <View style={styles.container}>
             <Text>React Native Geolocation</Text>
-            <View
-                style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-                <Button title="Get Location"
-                        onPress={getLocation}
-                />
+            <View style={styles.viewStyle}>
+                <Button title="Get Location" onPress={getLocation}/>
             </View>
             <Text>Latitude: {viewLocation.latitude} </Text>
             <Text>Longitude: {viewLocation.longitude} </Text>
-            <View
-                style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-                <Button
-                    title="Send Location"
-                    onPress={uploadLocation}
-                />
+            <View style={styles.viewStyle}>
+                <Button title="Send Location" onPress={uploadLocation}/>
             </View>
         </View>
     );
@@ -78,6 +90,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    viewStyle: {
+        marginTop: 10,
+        padding: 10,
+        borderRadius: 10,
+        width: '40%'
     },
 });
 
